14a8b54658d98e6d1eafd29f2ba93d0baf1364c6
Mayra Ruiz <mey.ruiz27@gmail.com>
2014-10-22 13:21:00 -0500

    Initial Commit

---
+++ Ejercicios-C/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Ejercicios-C
+//
+//  Created by Mayra Ruiz on 22/10/14.
+//  Copyright (c) 2014 Mayra Ruiz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
